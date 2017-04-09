bb799669e9b6bc74b5afdd6a8dbca687dbfc21d7
jaarellano@csumb.edu <jaarellano@csumb.edu>
2017-04-09 20:30:10 +0000

    GUI ready

--- project3.py
+++ project3.py
@@ -1,16 +1,60 @@
-
 import speech_recognition as sr
-# obtain audio from the Microphone
+from tkinter import *
+
+def speak():
+    i = -1
+    x = 1
+    y = 0
+    while i < 0:
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+            s = r.recognize_google(audio)
+            #print("Say something: ")
+        
+        try:
+            response = "you said '" + s + "'"
+            left = Label(root, text= response)
+            left.grid(row = x, column = y)
+            x+=1
+            #print ("you said '" + r.recognize_google(audio) + "'")
+        except sr.UnkownValueError:
+            response = "could not understand audio"
+            left = Label(root, text= response)
+            left.grid(row = x, column = y)
+            x+=1
+            #print ("could not understand audio")
+        except sr.RequestError as e:
+            response = "Could not request results from Google Speech Recognition service; {0}".format(e)
+            left = Label(root, text= response)
+            left.grid(row = x, column = y)
+            x+=1
+            #print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        if "how are you" in s:
+            response = "I am fine"
+            left = Label(root, text= response)
+            left.grid(row = x, column = y)
+            x+=1
+            #print ("I am fine")
+        if "exit" or "quit"  or "leave" in s:
+            response = "bye"
+            left = Label(root, text= response)
+            left.grid(row = x, column = y)
+            
+            i+=1
+
+root = Tk()
+root.title("digital assistant")
+
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH$
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-	print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+prompt = "Press to speak: "
+labelText = StringVar()
+labelText.set(prompt)
+label = Label(root, textvariable = labelText)
+
+speak_button = Button(root, text = "", width = 5 ,command = speak)
+
+label.grid(row = 0, column = 0)
+speak_button.grid(row = 0, column = 3)
+
+root.mainloop()
