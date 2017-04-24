a7df5ebbd6050307611e5f04c6632f5ea1ae7170
jashmaebaculpo <jbaculpo@csumb.edu>
2017-04-24 14:26:12 -0700

    Speech to Text and Text to Speech
    I added google Text to Speech so the program will tell you what you said. I will have to fix this so that it does not run on a while loop, and only runs once. To use this program, you have to download google's speech to text, text to speech and pygame in Python3.

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,16 +1,27 @@
-
 import speech_recognition as sr
+from gtts import gTTS # google's Text To Speech
+from pygame import mixer # opens and plays the mp3 file for gTTS
+import os
+
 # obtain audio from the Microphone
 r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Say something!")
     audio = r.listen(source)
+# recognize speech using Google Speech Recognition
 try:
     # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH$
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    data = r.recognize_google(audio)
+    data = ' '.join(word[0].upper() + word[1:] for word in data.split())
+    tts = gTTS(text = data, lang = 'en')
+    tts.save("good.mp3")
+    mixer.init()
+    mixer.music.load('/Users/noobkittyyuki/Desktop/good.mp3')
+    mixer.music.play()
+    print("You said '" + r.recognize_google(audio)+ "")
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
-	print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
