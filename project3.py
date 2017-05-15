b759e56b144f50bdeee61029d44fc5f38c59212f
jaarellano@csumb.edu <jaarellano@csumb.edu>
2017-05-15 19:40:51 +0000

    done

--- project3.py
+++ project3.py
@@ -1,3 +1,24 @@
+'''#################################################
+Final Project Digital Assistant
+Team Members: Jay Arellano, Jashmae Baculpo, Pricsilla Nunez
+Class: CST-205
+link for GitHub: https://github.com/jayarellano6/CST-205-Team_41
+link for c9: https://ide.c9.io/jayarellano/cst_205_project_1
+TA: Coleman
+Filename: project3.py
+last updated: May 15, 2017
+IDE used: IDLE (python 3.6 64-bit)
+
+Description: This project allows for the user press the speak button to talk to it and ask
+it to perform one of the tasks it is currently capable of performing. The user can ask digital
+assistant to send them a reminder, set up a google calendar event or play a song, along
+with other conversational phrases like hello, how are you, or 'tell me a joke'
+
+How to use: to use the application simply press the speak button and ask the digital
+assistant to perform one of the tasks described above.
+#################################################'''
+
+############Libraries Used############
 from __future__ import print_function
 import speech_recognition as sr
 from tkinter import *
@@ -17,7 +38,10 @@ from oauth2client.file import Storage
 import datetime
 import sys
 import webbrowser
+#####################################
 
+#########Google's get_credentials Quick Start for google calendar API#########
+#Source: https://developers.google.com/google-apps/calendar/quickstart/python
 try:
     import argparse
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
@@ -58,39 +82,45 @@ def get_credentials():
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
+##############################################################################
 
-###############################################
-'''credentials = get_credentials()
-http = credentials.authorize(httplib2.Http())
-service = discovery.build('calendar', 'v3', http=http)
 
-created_event = service.events().quickAdd(calendarId='primary',text='class on April 27th 10am-11:25am').execute()
-
-print (created_event['id'])'''
-###############################################
-
+############Function to get User's speech############
 def speak():
     j = randint(0,10000000)
     k = randint(0,10000000)
     x = 1
     y = 0
-    with sr.Microphone() as source:
+    with sr.Microphone() as source: #using microphone 
         audio = r.listen(source)
-        s = r.recognize_google(audio)
+        s = r.recognize_google(audio) #recognizing the audio
         
-    response = "you said '" + s + "'"
+    response = "you said '" + s + "'"#telling the user what they said
     left = Label(root, text= response)
     left.grid(row = x, column = y)
     tts = gTTS(text=response, lang='en')
-    tts.save(str(j) + ".mp3")
+    tts.save(str(j) + ".mp3")#saving the response audio file
     os.system("mpg321 " + str(j) + ".mp3")
     mixer.init()
     mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(j) + ".mp3")
-    mixer.music.play()
+    mixer.music.play()#playing the response audio file
     time.sleep(2)
     x+=1
     
-    if "hello" in s:
+    if "hello" in s:#if the user says hello in their sentence
+        t = randint(0, 5)
+        rs = ["hey", "hello to you", "whats up", "hi", "suh dude", "hey you"] #list of responses that gets generated randomly
+        response = rs[t]
+        left = Label(root, text= response)
+        left.grid(row = x, column = y)
+        tts = gTTS(text=response, lang='en')
+        tts.save(str(k) + ".mp3")
+        os.system("mpg321 " + str(k) + ".mp3")
+        mixer.init()
+        mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
+        mixer.music.play()
+        x+=1
+    elif "hi" in s:#if the user says hello in their sentence
         t = randint(0, 5)
         rs = ["hey", "hello to you", "whats up", "hi", "suh dude", "hey you"]
         response = rs[t]
@@ -103,8 +133,21 @@ def speak():
         mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
         mixer.music.play()
         x+=1
-    elif "reminder" in s:
-        def speak2():
+    elif "what's your name" in s:#if the user asks digital assistant what her name is
+        t = randint(0, 5)
+        rs = ["i'm not siri", "i'm not alexa", "i don't have a name yet", "digital assistant", "its like siri but not really", "i am groot"]
+        response = rs[t]
+        left = Label(root, text= response)
+        left.grid(row = x, column = y)
+        tts = gTTS(text=response, lang='en')
+        tts.save(str(k) + ".mp3")
+        os.system("mpg321 " + str(k) + ".mp3")
+        mixer.init()
+        mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
+        mixer.music.play()
+        x+=1
+    elif "reminder" in s:#if user asks digial assistant to set a reminder
+        def speak2():#response function
             j = randint(0,10000000)
             k = randint(0,10000000)
             x = 1
@@ -124,8 +167,8 @@ def speak():
             mixer.music.play()
             time.sleep(2)
             x+=1
-            if "yes" in s:
-                def speak3():
+            if "yes" in s: #if user responds to digital assistnant with 'yes'
+                def speak3():#reinder informaton fuction
                     j = randint(0,10000000)
                     k = randint(0,10000000)
                     x = 1
@@ -144,6 +187,7 @@ def speak():
                     mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(j) + ".mp3")
                     mixer.music.play()
                     time.sleep(2)
+                    ###Sending the email reminder###
                     server = smtplib.SMTP('smtp.gmail.com', 587)
                     server.starttls()
                     server.login("cst205.digital.assistant@gmail.com", "CST205jpj")
@@ -151,8 +195,9 @@ def speak():
                     msg = s
                     server.sendmail("cst205.digital.assistant@gmail.com", "jayarellano1129@gmail.com", msg)
                     server.quit()
+                    ################################
                     x+=1
-                response = "ok i will do that, say the message you want me to send:"
+                response = "ok i will do that, say the message you want me to send:"#asking user what they want in the reminder
                 left = Label(root, text= response)
                 left.grid(row = x, column = y)
                 tts = gTTS(text=response, lang='en')
@@ -163,8 +208,8 @@ def speak():
                 mixer.music.play()
                 x+=1
                 time.sleep(4)
-                speak3()
-            if "no" in s:
+                speak3()#calling the reminder information fucntion
+            if "no" in s:#if user says 'no' to the reminder
                 response = "ok nevermind"
                 left = Label(root, text= response)
                 left.grid(row = x, column = y)
@@ -176,7 +221,7 @@ def speak():
                 mixer.music.play()
                 x+=1 
       
-        response = "you want me to set a reminder?"
+        response = "you want me to set a reminder?"#asking the user if she should set a reminder
         left = Label(root, text= response)
         left.grid(row = x, column = y)
         tts = gTTS(text=response, lang='en')
@@ -187,9 +232,9 @@ def speak():
         mixer.music.play()
         x+=1
         time.sleep(2.5)
-        speak2()
-    elif "calendar" in s:
-        def speak2():
+        speak2()#calling response function
+    elif "calendar" in s:#if the user wants to set a google calendar event 
+        def speak2():#response fuction
             j = randint(0,10000000)
             k = randint(0,10000000)
             x = 1
@@ -209,8 +254,8 @@ def speak():
             mixer.music.play()
             time.sleep(2)
             x+=1
-            if "yes" in s:
-                def speak3():
+            if "yes" in s:#if user responds to digital assistant with 'yes'
+                def speak3():#calendar information function
                     j = randint(0,10000000)
                     k = randint(0,10000000)
                     x = 1
@@ -229,13 +274,15 @@ def speak():
                     mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(j) + ".mp3")
                     mixer.music.play()
                     time.sleep(2)
+                    ###Setting the calendar event using quickAdd fuction from API###
                     credentials = get_credentials()
                     http = credentials.authorize(httplib2.Http())
                     service = discovery.build('calendar', 'v3', http=http)
 
                     created_event = service.events().quickAdd(calendarId='primary',text=s).execute()
 
-                    print (created_event['id'])
+                    print (created_event['id'])#making sure the event was created
+                    ################################################################
                     x+=1
                 response = "ok i will do that, say the event, date and time you want me to create:"
                 left = Label(root, text= response)
@@ -249,7 +296,7 @@ def speak():
                 x+=1
                 time.sleep(5)
                 speak3()
-            if "no" in s:
+            if "no" in s:#if user responds to digital assistant with 'no'
                 response = "ok nevermind"
                 left = Label(root, text= response)
                 left.grid(row = x, column = y)
@@ -261,7 +308,7 @@ def speak():
                 mixer.music.play()
                 x+=1 
       
-        response = "you want me to set a google calendar event?"
+        response = "you want me to set a google calendar event?"#asking the user if they want to set a google calendar event
         left = Label(root, text= response)
         left.grid(row = x, column = y)
         tts = gTTS(text=response, lang='en')
@@ -272,8 +319,8 @@ def speak():
         mixer.music.play()
         x+=1
         time.sleep(3)
-        speak2()
-    elif "how are you" in s:
+        speak2()#calling the response fuction
+    elif "how are you" in s:#if the user says 'how are you' in their sentence
         t = randint(0, 5)
         rs = ["i am fine", "i don't want to talk about it", "i'm great!", "terrible", "i don't know how i am", "i cant't tell"]
         response = rs[t]
@@ -286,8 +333,8 @@ def speak():
         mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
         mixer.music.play()
         x+=1
-    elif "exit" in s:
-        response = "bye"
+    elif "exit" in s:#if the user says 'exit' in their sentence
+        response = "see ya"
         left = Label(root, text= response)
         left.grid(row = x, column = y)
         tts = gTTS(text=response, lang='en')
@@ -296,9 +343,10 @@ def speak():
         mixer.init()
         mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
         mixer.music.play()
-    elif "joke" in s:
+        root.destroy()#kills program
+    elif "joke" in s:#if the user says 'joke' in their sentence
         t = randint(0, 5)
-        rs = ["i don't have any", "i can't", "a joke", "you", "knock knock, who's there? no one", "i cannot understand 'joke'"]
+        rs = ["What kind of bagel can fly? A plain bagel", "Where do animals go when their tails fall off? a retail store", "a joke", "you", "how does a train eat? it goes chew chew", "whats forest gump's password? 1 forest 1"]
         response = rs[t]
         left = Label(root, text= response)
         left.grid(row = x, column = y)
@@ -308,8 +356,29 @@ def speak():
         mixer.init()
         mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
         mixer.music.play()
-    else:
-        def speak2():
+    elif "song" in s:#if the user says 'song' in their sentence
+        t = randint(0, 6)
+        rs = ["05 - Booty From A Distance.mp3", "09 - Bernie Sanders.mp3",
+              "Drake - Hotline Bling.mp3", "Gucci Mane Both feat. Drake Official Audio.mp3",
+              "Kendrick Lamar - HUMBLE..mp3", "Travis Scott - goosebumps ft. Kendrick Lamar.mp3",
+              "J. Cole - Neighbors.mp3"]#7 possible songs to play
+        response = "ok here you go"
+        left = Label(root, text= response)
+        left.grid(row = x, column = y)
+        tts = gTTS(text=response, lang='en')
+        tts.save(str(k) + ".mp3")
+        os.system("mpg321 " + str(k) + ".mp3")
+        mixer.init()
+        mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
+        mixer.music.play()
+        time.sleep(2)
+        mixer.init()
+        mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/pythonMusic/" + rs[t])
+        mixer.music.play(loops=0, start=25)
+        time.sleep(13)
+        mixer.music.stop()
+    else:#if the user says a sentence with a phrase that it did not have a functionality for
+        def speak2():#response fucntion
             j = randint(0,10000000)
             k = randint(0,10000000)
             x = 1
@@ -329,7 +398,7 @@ def speak():
             mixer.music.play()
             time.sleep(2)
             x+=1
-            if "yes" in web:
+            if "yes" in web:#if user responds to digital assistant with 'yes'
                 response = "ok opening google now"
                 left = Label(root, text= response)
                 left.grid(row = x, column = y)
@@ -340,9 +409,11 @@ def speak():
                 mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
                 mixer.music.play()
                 time.sleep(2)
+                ###searching unrecognized sentence on google###
                 url = "https://www.google.co.in/search?q=" +(s)+ "&oq="+(s)+"&gs_l=serp.12..0i71l8.0.0.0.6391.0.0.0.0.0.0.0.0..0.0....0...1c..64.serp..0.0.0.UiQhpfaBsuU"
-                webbrowser.open_new(url)
-            if "no" in web:
+                webbrowser.open_new(url)#opening the search results
+                ###############################################
+            if "no" in web:#if user responds to digital assistant with 'yes'
                 response = "ok nevermind"
                 left = Label(root, text= response)
                 left.grid(row = x, column = y)
@@ -353,7 +424,7 @@ def speak():
                 mixer.music.load("/Users/jayarellano/AppData/Local/Programs/Python/Python36/" + str(k) + ".mp3")
                 mixer.music.play()
                 x+=1
-        response = "Huh, I dont have any results for '" + s + "' do you want to search the web? " 
+        response = "Huh, I dont have any results for '" + s + "' do you want to search google? " #asking user if they want to search google for unrecongnized sentence
         left = Label(root, text= response)
         left.grid(row = x, column = y)
         tts = gTTS(text=response, lang='en')
@@ -364,22 +435,23 @@ def speak():
         mixer.music.play()
         x+=1
         time.sleep(5)
-        speak2()
+        speak2()#calling response function
 
+####setting up window for digital assistant####
 root = Tk()
 root.title("digital assistant")
-root.configure(background = "navy")
 
 r = sr.Recognizer()
 
 prompt = "Press to speak:  "
 labelText = StringVar()
 labelText.set(prompt)
-label = Label(root, bg= "navy", textvariable = labelText, fg= "White")
+label = Label(root, textvariable = labelText,)#displaying text on window
 
-speak_button = Button(root, text = "\(^.^)/", width = 6 ,command = speak)
+speak_button = Button(root, text = "\(^.^)/", width = 6 ,command = speak)#setting up button on window with speech function
 
 label.grid(row = 0, column = 0)
-speak_button.grid(row = 0, column = 3)
+speak_button.grid(row = 0, column = 3)#positioning the button
+###############################################
 
 root.mainloop()
